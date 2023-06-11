09a2aff2dd90c38138cd98e292f744617c72904f
Rahul Ghosal <rdghosal@gmail.com>
2023-06-11 08:12:43 -0400

    feat: prompt

--- ts/app.ts
+++ ts/app.ts
@@ -1,28 +1,45 @@
 import * as fs from "fs";
+import * as readline from "readline";
 
 class Lox {
     static main(args: string[]): void {
         if (args.length > 1)  {
-            console.log("Usage: jlox [sript]");
+            console.log("Usage: jlox [script]");
             return process.exit(64);
         } else if (args.length === 1) {
-            runFile(args[0]);
-        } else if (args.length === 1) {
-            runPrompt();
+            Lox.runFile(args[0]);
+        } else if (!args.length) {
+            Lox.runPrompt();
         }
     };
 
     static runFile(path: string) {
-        fs.readFile(path, (err: NodeJS.ErrnoException, data: Buffer) => {
+        fs.readFile(path, (err: NodeJS.ErrnoException | null, data: Buffer) => {
             if (err) {
                 throw err;
             } else {
-                String.fromCharCode(data);
-
+                data.forEach((byte: number) => { 
+                    Lox.run(String.fromCharCode(byte)) 
+                });
             }
         });
     };
 
-    static run() { // TODO }
+    static runPrompt() {
+        // `readline` handles Ctrl-D and closes stream by itself.
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        rl.setPrompt('> ');
+        rl.prompt();
+        rl.on('line', (line: string) => {
+            console.log(`received ${line}`);
+            rl.prompt();
+        });
+    };
+
+    static run(ch: string) { /* TODO */ }
 };
 
+Lox.main([]);
