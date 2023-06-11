31758148aceae15f91641d8f520576d5623a5854
Rahul Ghosal <rdghosal@gmail.com>
2023-06-11 12:04:09 -0400

    feat: string literals

--- ts/scanner.ts
+++ ts/scanner.ts
@@ -76,9 +76,31 @@ class Scanner {
             case "\n":
                 this.line++;
                 break;
+            case '"': this.string(); break;
             default: Lox.error(this.line, "Unexpected character."); break;
         }
     };
+
+    private string(): void {
+        while (this.peek() !== '"' && !this.isAtEnd()) {
+            if (this.peek() === "\n") {
+                this.line++;
+            }
+            this.advance();
+        }
+        if (this.isAtEnd()) {
+            Lox.error(this.line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        this.advance();
+
+        // Trim the surrounding quotes.
+        const value: string = this.source.substring(this.start + 1,
+                                                    this.current - 1);
+        this.addToken(TokenType.STRING, value);
+    }
 
     private match(expected: string): boolean {
         // Advance only if there's a match.
@@ -107,7 +129,7 @@ class Scanner {
         return this.source.charAt(this.current++);
     };
 
-    private addToken(type: TokenType, literal: object | null = null): void {
+    private addToken(type: TokenType, literal: any | null = null): void {
         const text: string = this.source.substring(this.start, this.current);
         this.tokens.push(new Token(type, text, literal, this.line));
     };
