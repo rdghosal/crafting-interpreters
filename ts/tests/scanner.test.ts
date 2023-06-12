83921c9fb85f7f7f09a5c321966f20aaf9dafdaf
Rahul Ghosal <rdghosal@gmail.com>
2023-06-12 07:50:08 -0400

    refactor: literal values

--- ts/tests/scanner.test.ts
+++ ts/tests/scanner.test.ts
@@ -32,34 +32,34 @@ test("test Scanner().scanTokens() complete", function() {
     // 1. Arrange
     const source: string =`myVar = 5;
         fun compareSum(x, y) {
-            print(x + y > my_var);
+            print(x + y > myVar);
         };
         `;
     var tokens = [
         new Token(TokenType.IDENTIFIER, "myVar", "myVar", 1),
         new Token(TokenType.EQUAL, "=", null, 1),
-        new Token(TokenType.NUMBER, "5", "5", 1),
+        new Token(TokenType.NUMBER, "5", parseFloat("5"), 1),
         new Token(TokenType.SEMICOLON, ";", null, 1),
         new Token(TokenType.FUN, "fun", "", 2),
         new Token(TokenType.IDENTIFIER, "compareSum", "compareSum", 2),
         new Token(TokenType.LEFT_PAREN, "(", "", 2),
-        new Token(TokenType.IDENTIFIER, "x", "x", 2),
+        new Token(TokenType.IDENTIFIER, "x", "", 2),
         new Token(TokenType.COMMA, ",", "", 2),
-        new Token(TokenType.IDENTIFIER, "y", "y", 2),
+        new Token(TokenType.IDENTIFIER, "y", "", 2),
         new Token(TokenType.RIGHT_PAREN, ")", "", 2),
         new Token(TokenType.LEFT_BRACE, "{", "", 2),
         new Token(TokenType.PRINT, "print", "", 3),
         new Token(TokenType.LEFT_PAREN, "(", "", 3),
-        new Token(TokenType.IDENTIFIER, "x", "x", 3),
+        new Token(TokenType.IDENTIFIER, "x", "", 3),
         new Token(TokenType.PLUS, "+", "", 3),
-        new Token(TokenType.IDENTIFIER, "y", "y", 3),
+        new Token(TokenType.IDENTIFIER, "y", "", 3),
         new Token(TokenType.GREATER, ">", "", 3),
         new Token(TokenType.IDENTIFIER, "myVar", "myVar", 3),
         new Token(TokenType.RIGHT_PAREN, ")", "", 3),
         new Token(TokenType.SEMICOLON, ";", "", 3),
         new Token(TokenType.RIGHT_BRACE, "}", "", 4),
         new Token(TokenType.SEMICOLON, ";", "", 4),
-        new Token(TokenType.EOF, "", null, 1),
+        new Token(TokenType.EOF, "", null, 4),
     ];
 
     // 2. Act
@@ -67,6 +67,7 @@ test("test Scanner().scanTokens() complete", function() {
 
     // 3. Assert
     scanned.forEach((s, i) => {
+        console.log(s)
         expect(s.type).toBe(tokens[i].type);
     });
 });
